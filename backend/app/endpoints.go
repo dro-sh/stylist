a2309643f371c2ad1d9cf72f758a73ed7ca916de
dro-sh <drozhzhin200@yandex.ru>
2023-03-25 14:08:15 +0300

    1.1.1

--- backend/app/endpoints.go
+++ backend/app/endpoints.go
@@ -1,20 +1,27 @@
 package app
 
 import (
+	"crypto/md5"
+	"fmt"
 	"net/http"
 
 	"github.com/dro-sh/stylist/backend/app/lib"
 )
 
-// Message ...
 type Message struct {
-	Message string `json:"message"`
+	Message string `json:"token"`
 }
 
-// HelloWorld ...
+// Login performs login to service by login and password
 func Login(w http.ResponseWriter, r *http.Request) {
+	login := r.URL.Query().Get("login")
+	password := r.URL.Query().Get("password")
+
+	data := []byte(login + password)
+
 	res := lib.Response{ResponseWriter: w}
 
-	m := Message{"Hello User!"}
+	token := fmt.Sprintf("%x", md5.Sum(data))
+	m := Message{string(token)}
 	res.SendOK(m)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/cors v1.8.3
 	github.com/sirupsen/logrus v1.9.0
